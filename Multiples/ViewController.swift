483ea793ff8665062c40b6a2b9351335212b3c36
Robert McBryde <rob@yavi.co>
2016-01-06 22:36:45 +0000

    MVP of the app. Not the prettiest but works, needs to be refactored.

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var multiple = 0
+    var currentTotal = 0
+    let maxValue = 20
+    var sum = 0
+    
+    // Outlets
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var multipleInput: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var addLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    
+    @IBAction func playButtonPressed(sender: UIButton) {
+        startGame()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addButtonPressed(sender: UIButton) {
+        if (sum < maxValue) {
+            sum = currentTotal + multiple
+            addLabel.text = "\(currentTotal) + \(multiple) = \(sum)"
+            currentTotal = sum
+        } else {
+            restartGame()
+        }
     }
-
+    
+    func startGame() {
+        if multipleInput.text != nil && multipleInput.text != "" {
+            multiple = Int(multipleInput.text!)!
+            
+            logoImage.hidden = true
+            multipleInput.hidden = true
+            playButton.hidden = true
+            
+            addLabel.hidden = false
+            addButton.hidden = false
+        }
+    }
+    
+    func restartGame() {
+        
+            multiple = 0
+            multipleInput.text = ""
+            
+            logoImage.hidden = false
+            multipleInput.hidden = false
+            playButton.hidden = false
+            
+            addLabel.hidden = true
+            addButton.hidden = true
+    }
 
 }
 
